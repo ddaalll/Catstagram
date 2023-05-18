f51336fda5651604a22b08d05931df875e947d02
Dalhyun-Nam <115088160+ddaalll@users.noreply.github.com>
2023-05-19 01:54:23 +0900

    commit

--- Catstagram/Catstagram/RegisterViewController.swift
+++ Catstagram/Catstagram/RegisterViewController.swift
@@ -8,22 +8,60 @@
 import UIKit
 
 class RegisterViewController: UIViewController {
+    // MARK: - Properties
+    
+    var isValidEmail = false
+    var isValidName = false
+    var isValidNickname = false
+    var isValidPassword = false
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var nicknameTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    var textField: [UITextField] {
+        [emailTextField, nameTextField, nicknameTextField, passwordTextField]
+    }
+    
+    // MARK: - LifeCycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        setupTextField()
+        
+    }
+    // MARK: - Actions
+    @objc func textFieldEditingChanged(_ sender: UITextField) {
+        let text = sender.text ?? ""
+        
+        switch sender {
+        case emailTextField:
+            print("email")
+        case nameTextField:
+            print("name")
+        case nicknameTextField:
+            print("nickname")
+        case passwordTextField:
+            print("password")
+        default:
+            fatalError("Missing TextField...")
+        }
     }
     
+    
+    // MARK: - Helpers
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setupTextField() {
+        textField.forEach { tf in
+            tf.addTarget(self, action: #selector(textFieldEditingChanged(_:)), for: .editingChanged)
+            
+        }
+        
     }
-    */
 
 }
