06c9ac19f639f0d47dde9393c635b936565133e4
Dalhyun-Nam <115088160+ddaalll@users.noreply.github.com>
2023-05-23 02:01:40 +0900

    commit

--- Catstagram/Catstagram/RegisterViewController.swift
+++ Catstagram/Catstagram/RegisterViewController.swift
@@ -10,10 +10,29 @@ import UIKit
 class RegisterViewController: UIViewController {
     // MARK: - Properties
     
-    var isValidEmail = false
-    var isValidName = false
-    var isValidNickname = false
-    var isValidPassword = false
+    var isValidEmail = false {
+        didSet{
+            self.validateUserInfo()
+        }
+    }
+    var isValidName = false {
+        didSet{
+            self.validateUserInfo()
+        }
+    }
+    var isValidNickname = false {
+        didSet{
+            self.validateUserInfo()
+        }
+    }
+    var isValidPassword = false {
+        didSet{
+            self.validateUserInfo()
+        }
+    }
+    
+    @IBOutlet weak var signupButton: UIButton!
+    
     
     @IBOutlet weak var emailTextField: UITextField!
     
@@ -23,7 +42,7 @@ class RegisterViewController: UIViewController {
     
     @IBOutlet weak var passwordTextField: UITextField!
     
-    var textField: [UITextField] {
+    var textFields: [UITextField] {
         [emailTextField, nameTextField, nicknameTextField, passwordTextField]
     }
     
@@ -41,27 +60,60 @@ class RegisterViewController: UIViewController {
         
         switch sender {
         case emailTextField:
-            print("email")
+            self.isValidEmail = text.isValidEmail()
+            
         case nameTextField:
-            print("name")
+            self.isValidName = text.count > 2
+            
         case nicknameTextField:
-            print("nickname")
+            self.isValidNickname = text.count > 2
         case passwordTextField:
-            print("password")
+            self.isValidPassword = text.isValidPassword()
         default:
             fatalError("Missing TextField...")
         }
+        
     }
+    
     
     
     // MARK: - Helpers
 
     func setupTextField() {
-        textField.forEach { tf in
+        textFields.forEach { tf in
             tf.addTarget(self, action: #selector(textFieldEditingChanged(_:)), for: .editingChanged)
             
         }
         
     }
+    
+    // 사용자가 입력한 회원정보를 확인하고 -> UI 구성을 확인
+    private func validateUserInfo() {
+        
+        if isValidEmail && isValidName && isValidNickname && isValidPassword {
+            self.signupButton.backgroundColor = UIColor(red: 65/255, green: 147/255, blue: 239/255, alpha: 1)
+        } else {
+            self.signupButton.backgroundColor = UIColor(red: 198/255, green: 222/255, blue: 250/255, alpha: 1)
+        }
+    }
 
+}
+
+// 정규 표현식
+
+extension String {
+    // 대문자, 소문자, 특수문자, 숫자 8자 이상일때 -> true
+    func isValidPassword() -> Bool {
+        let regularExpression = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[$@$!%*?&])[A-Za-z\\d$@$!%*?&]{8,}"
+        let passwordValidation = NSPredicate.init(format: "SELF MATCHES %@", regularExpression)
+        
+        return passwordValidation.evaluate(with: self)
+    }
+    
+    //
+    func isValidEmail() -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailTest = NSPredicate(format: "SELF MATCHES %@", emailRegEx)
+        return emailTest.evaluate(with: self)
+    }
 }
